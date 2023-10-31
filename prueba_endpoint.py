23463ec3e3d8578e0fd7d761fee8abb07e0e99ae
sebasga79 <sebasga_79@yahoo.com>
2023-10-30 19:23:49 -0500

    Adding prueba_endpoint.py

---
+++ prueba_endpoint.py
@@ -0,0 +1,18 @@
+import requests
+
+url  = 'https://deployment-intro-ml-service-arcrj4sdba-uc.a.run.app'
+
+json = {
+        'opening_gross': 8330681,
+        'screens': 2271,
+        'production_budget': 13000000,
+        'title_year': 1999,
+        'aspect_ratio': 1.85,
+        'duration': 97,
+        'cast_total_facebook_likes': 37907,
+        'budget': 16000000,
+        'imdb_score': 7.2
+        }
+
+breakpoint()
+
